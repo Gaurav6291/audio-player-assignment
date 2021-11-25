fd9d09c3d33ec13ace313488347d4c01b09b0736
Gaurav <parmargaurav1234@gmail.com>
2021-11-25 11:58:05 +0530

    basic layout

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
-context.fillRect(65, 40, 50, 50);
+for(let i=0; i<50; i++){
+    if(i%2===0){
+context.fillRect(i*5+10, Math.ceil(Math.random()*40)+60, 5, Math.ceil(Math.random()*140)+20);
+    }
+}
 // context.stroke();
